3bc1809e3cf3ee240675db63d3ab391d86167b65
anauskadutta <60913023+anauskadutta@users.noreply.github.com>
2023-10-12 19:15:50 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,16 +1,16 @@
-scanJson=$(gh api \
+scanJson='$(gh api \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        /repos/anauskadutta/sample1/code-scanning/alerts)
+        /repos/anauskadutta/sample1/code-scanning/alerts)'
 
 echo "The list of code scan alerts is as follows: $scanJson"
 
 # alertList=$(echo "$scanJson" | jq -c -r '.[]')
 
-for alert in $scanJson; do
+for alert in ${scanJson[@]}; do
         # echo "Alerts are printed here: $alert"
-        state=$(echo "$scanJson" | jq -c -r '.[]' | '.state')
-        # echo "State: $state"
+        state=$(echo $alert | jq -r '.[]' | '.state')
+        echo "State: $state"
         # if [ $state=="open" ]; then
         #         issueTitle=${alert.most_recent_instance.message.text}
         #         echo "Title: $issueTitle"
