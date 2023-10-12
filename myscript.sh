6eaf1f4069b3e31f40bda50a9c6d34105a69d6df
anauskadutta <60913023+anauskadutta@users.noreply.github.com>
2023-10-12 19:17:19 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,7 @@
-scanJson='$(gh api \
+scanJson=$(gh api \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        /repos/anauskadutta/sample1/code-scanning/alerts)'
+        /repos/anauskadutta/sample1/code-scanning/alerts)
 
 echo "The list of code scan alerts is as follows: $scanJson"
 
