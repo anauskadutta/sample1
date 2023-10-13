33b1e2be1ccd6cd836ed6914a3f97bf50b66941f
anauskadutta <60913023+anauskadutta@users.noreply.github.com>
2023-10-13 11:29:12 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -5,21 +5,28 @@ scanJson=$(gh api \
 
 # echo "The list of code scan alerts is as follows: $scanJson"
 
-for alert in $(echo "$scanJson" | jq -r '.[] | @base64'); do
-        _jq() {
-                echo ${alert} | base64 --decode | jq -r ${1}
-        }
-        state=$(_jq '.state')
-        # echo "Alerts are printed here: $alert"
-        # state=$(echo $alert | jq -r '.state')
-        # echo "State: $state"
-        if [[ "$state" == "open" ]]; then
-                issueTitle="$(_jq '.most_recent_instance.message.text')"
-                issueBody=$(_jq '.html_url')
-                echo "State: $state"
-                echo "Title: $issueTitle"
-                echo "Body: $issueBody"
-                ghIssue=$(gh issue create --title "$issueTitle" --body "$issueBody")
-                echo "GitHub issue created: $ghIssue"
-        fi
-done
+issueJson=$(gh api \
+        -H "Accept: application/vnd.github+json" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        /repos/anauskadutta/sample1/issues)
+
+echo "List of GitHub Issues: $issueJson"
+
+# for alert in $(echo "$scanJson" | jq -r '.[] | @base64'); do
+#         _jq() {
+#                 echo ${alert} | base64 --decode | jq -r ${1}
+#         }
+#         state=$(_jq '.state')
+#         # echo "Alerts are printed here: $alert"
+#         # state=$(echo $alert | jq -r '.state')
+#         # echo "State: $state"
+#         if [[ "$state" == "open" ]]; then
+#                 issueTitle="$(_jq '.most_recent_instance.message.text')"
+#                 issueBody=$(_jq '.html_url')
+#                 echo "State: $state"
+#                 echo "Title: $issueTitle"
+#                 echo "Body: $issueBody"
+#                 ghIssue=$(gh issue create --title "$issueTitle" --body "$issueBody")
+#                 echo "GitHub issue created: $ghIssue"
+#         fi
+# done
