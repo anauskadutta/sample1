93063c7b2fc2e59a0d85ede41f5de0d06d71eea4
anauskadutta <60913023+anauskadutta@users.noreply.github.com>
2023-10-19 19:55:24 +0530

    Update get-jira-issue-details.py

--- .github/scripts/get-jira-issue-details.py
+++ .github/scripts/get-jira-issue-details.py
@@ -2,26 +2,26 @@ import os
 import requests
 import json
 
-# store API url
-url = 'https://jsjiraapp.atlassian.net/rest/api/3/search'
+jira_base_url = os.environ['JIRA_BASE_URL']
+jira_username = os.environ['JIRA_USER_EMAIL']
+jira_token = os.environ['JIRA_API_TOKEN']
 
-username = os.environ['JIRA_USER_EMAIL']
-token = os.environ['JIRA_API_TOKEN']
+jira_rest_api = '{jira_base_url}/rest/api/3/search'
 
-params = {
+jira_params = {
   'jql': 'project=STP',
   'issuetype': 'Bug'
 }
 
 # Set up headers for the JIRA request
-headers = {
+jira_headers = {
     'Content-Type': 'application/json',
 }
 
 # Set up authentication for the JIRA request
-auth = (username, token)
+jira_auth = (jira_username, jira_token)
 
-issue_details_response = requests.get(url, params=params, headers=headers, auth=auth)
+issue_details_response = requests.get(jira_rest_api, params=jira_params, headers=jira_headers, auth=jira_auth)
 issue_json = issue_details_response.json()
 issue_list = issue_json['issues']
 issue_description_list = []
