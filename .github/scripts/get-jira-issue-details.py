69670bc4dbbe215f902aac0b4d4217d1e12e06a5
anauskadutta <60913023+anauskadutta@users.noreply.github.com>
2023-10-19 19:25:12 +0530

    Update get-jira-issue-details.py

--- .github/scripts/get-jira-issue-details.py
+++ .github/scripts/get-jira-issue-details.py
@@ -26,5 +26,5 @@ issue_json = issue_details_response.json()
 issue_list = issue_json['issues']
 
 for issue in issue_list:
-  issue_description = issue['fields']['description']['content']['content']['text']
+  issue_description = issue.fields
   print(issue_description)
