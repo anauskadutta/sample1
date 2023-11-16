b9f293370fa12dfddcf832da57e49dde5752db28
anauskadutta <60913023+anauskadutta@users.noreply.github.com>
2023-11-16 16:54:15 +0530

    Update get-jira-issue-details.py

--- .github/scripts/get-jira-issue-details.py
+++ .github/scripts/get-jira-issue-details.py
@@ -2,8 +2,8 @@ import os
 import requests
 import json
 
-# jira_username = os.environ['JIRA_USER_EMAIL']
-# jira_token = os.environ['JIRA_API_TOKEN']
+jira_username = os.environ['JIRA_USER_EMAIL']
+jira_token = os.environ['JIRA_API_TOKEN']
 
 # jira_url = 'https://jsjiraapp.atlassian.net/rest/api/3/search'
 
@@ -12,31 +12,31 @@ jira_url = f"{jira_base_url}rest/api/3/search"
 
 print(jira_url)
 
-# jira_params = {
-#   'jql': 'project=STP',
-#   'issuetype': 'Bug'
-# }
+jira_params = {
+  'jql': 'project=STP',
+  'issuetype': 'Bug'
+}
 
-# # Set up headers for the JIRA request
-# jira_headers = {
-#     'Content-Type': 'application/json',
-# }
+# Set up headers for the JIRA request
+jira_headers = {
+    'Content-Type': 'application/json',
+}
 
-# # Set up authentication for the JIRA request
-# jira_auth = (jira_username, jira_token)
+# Set up authentication for the JIRA request
+jira_auth = (jira_username, jira_token)
 
-# issue_details_response = requests.get(jira_url, params=jira_params, headers=jira_headers, auth=jira_auth)
-# issue_json = issue_details_response.json()
-# print(issue_json) # testing printing
-# # issue_list = issue_json['issues']
-# # issue_description_list = []
+issue_details_response = requests.get(jira_url, params=jira_params, headers=jira_headers, auth=jira_auth)
+issue_json = issue_details_response.json()
+print(issue_json) # testing printing
+# issue_list = issue_json['issues']
+# issue_description_list = []
 
-# # for issue in issue_list:
-# #   issue_description = issue["fields"]["description"]["content"][0]["content"][0]["text"]
-# #   issue_description_list.append(issue_description)
+# for issue in issue_list:
+#   issue_description = issue["fields"]["description"]["content"][0]["content"][0]["text"]
+#   issue_description_list.append(issue_description)
 
-# # if "https://github.com/anauskadutta/sample1/security/code-scanning/13" in issue_description_list:
-# #   print("Issue already exists")
+# if "https://github.com/anauskadutta/sample1/security/code-scanning/13" in issue_description_list:
+#   print("Issue already exists")
 
-# # else:
-# #   print("New issue")
+# else:
+#   print("New issue")
